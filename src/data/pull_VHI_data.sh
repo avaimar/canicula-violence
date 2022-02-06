cd3d9ccf81bc825244c0fca7d0b400c289ca1526
Miki Verma <meverma@stanford.edu>
2022-02-06 15:36:56 -0800

    fix 2010 url for pulling VHI data

--- src/data/pull_VHI_data.sh
+++ src/data/pull_VHI_data.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
-## Downloads VHI data for 2011-2020 for weeks 27-36 (June, July)
+## Downloads VHI data for 2010-2020 for weeks 27-36 (June, July)
 # Run this script from project directory :
 # > sh src/data/pull_VHI_data.sh
 
 cd data_raw_local/VHI
-
-for YEAR in {2011..2013}
+for YEAR in {2010..2011}
 do
-  [[ "$YEAR" < 2013 ]] && FILE_PREFIX=NP || FILE_PREFIX=npp
+  if ((YEAR <= 2010))
+  then
+      FILE_PREFIX=NN
+  elif ((YEAR <= 2012))
+  then
+      FILE_PREFIX=NP
+  else
+      FILE_PREFIX=npp
+  fi
+  #[[ "$YEAR" < 2013 ]] && FILE_PREFIX=NP || FILE_PREFIX=npp
   for WEEK_NUM in {27..35}
   do
-    # echo "$FILE_PREFIX"
     curl --remote-name https://www.star.nesdis.noaa.gov/pub/corp/scsb/wguo/data/Blended_VH_4km/geo_TIFF/VHP.G04.C07.${FILE_PREFIX}.P${YEAR}0${WEEK_NUM}.VH.VHI.tif
   done
 done
