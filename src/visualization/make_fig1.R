8d9784c32578df3f67ab3dd76cb37f4620229965
Miki Verma <meverma@stanford.edu>
2022-02-14 16:28:23 -0800

    add country boundaries to fig1

--- src/visualization/make_fig1.R
+++ src/visualization/make_fig1.R
@@ -12,15 +12,18 @@ processed_data_fpath <- file.path(gdrive_fpath, "Data", "Processed")
 figure_fpath <- file.path("results", "figures" )
 
 #2. Read in Data -----
+country_sf <- st_read(
+  file.path(processed_data_fpath, "country_outlines.geojson")
+)
 admin_bndry_sf <- st_read(
   file.path(processed_data_fpath, "cleaned_admin_boundaries.geojson"))
 vhi_df <- read.csv(
   file.path(processed_data_fpath, "Canicula_index_2010_2020.csv"))
-#convert canicula index to ordered factpr
+#convert canicula index to ordered factor
 vhi_df$Canicula_Label <- factor(vhi_df$Canicula_Label,
   levels = c("Extreme", "Severe", "Moderate", "Mild", "None"))
 hom_df <- read.csv(file.path(processed_data_fpath, "homicide_rates.csv"))
-hom_df <- hom_df[!duplicated(hom_df), ]
+# sum(duplicated(hom_df))
 
 #3. Join Canicula Data, Homicide Data, and Shapefiles -----
 canicula_sf <- left_join(
@@ -33,30 +36,46 @@ full_sf <- left_join(
   hom_df,
   by = c("Departamento", "Municipio", "Year"))
 
-y <- 2015
-pdf(file.path(figure_fpath, paste("homicide_rate_ceil", y, "pdf", sep = ".")))
-subset(full_sf, Year == y) %>%
-  ggplot() +
-    geom_sf(
-      aes(fill = pmin(hom_rate_100k, 200)), lwd = 0.1) +
-    scale_fill_viridis_c(option = "magma", na.value = "grey") +
-    guides(fill = guide_colorbar(
-      title = "Homicides per 100K",
-      label.theme = element_text(colour = "gray10", angle = 0, size = 8),
-      title.theme = element_text(colour = "gray10", angle = 0, size = 10))) +
-    theme_void()
+
+y <- 2015 #choosing 2015 to display
+#Homicide rate map (with max val of 200)
+pdf(file.path(figure_fpath, paste("Homicide_Rate_Ceil", y, "pdf", sep = ".")))
+ggplot() +
+  geom_sf(data = subset(full_sf, Year == y),
+    aes(fill = pmin(hom_rate_100k, 200)), lwd = 0.1) +
+  geom_sf(data = country_sf, fill = NA, lwd = 0.1, color = "white") +
+  scale_fill_viridis_c(option = "magma", na.value = "grey") +
+  guides(fill = guide_colorbar(
+    title = "Homicides per 100K",
+    label.theme = element_text(colour = "gray10", angle = 0, size = 8),
+    title.theme = element_text(colour = "gray10", angle = 0, size = 10))) +
+  theme_void()
 dev.off()
 
-
+#Canicula Index Map with Labels
 pdf(file.path(figure_fpath, paste("Canicula_Label", y, "pdf", sep = ".")))
-subset(full_sf, Year == y) %>%
-  ggplot() +
-    geom_sf(
-      aes(fill = Canicula_Label), lwd = 0) +
-    scale_fill_viridis_d(option = "magma", begin = 1, end = 0) +
-    guides(fill = guide_legend(
-      title = "Canicula Intensity", 
-      label.theme = element_text(colour = "gray10", angle = 0, size = 8),
-      title.theme = element_text(colour = "gray10", angle = 0, size = 10))) +
-    theme_void()
+ggplot() +
+  geom_sf(data = subset(full_sf, Year == y),
+    aes(fill = Canicula_Label), lwd = 0) +
+  geom_sf(data = country_sf, fill = NA, lwd = 0.1, color = "white") +
+  scale_fill_viridis_d(option = "magma", begin = 1, end = 0) +
+  guides(fill = guide_legend(
+    title = "Canicula Intensity", 
+    label.theme = element_text(colour = "gray10", angle = 0, size = 8),
+    title.theme = element_text(colour = "gray10", angle = 0, size = 10))) +
+  theme_void()
+dev.off()
+
+#Mean VHI (over 9 week period in summer) Map
+pdf(file.path(figure_fpath, paste("Canicula_Period_VHI", y, "pdf", sep = ".")))
+ggplot() +
+  geom_sf(data = subset(full_sf, Year == y),
+    aes(fill = mean_vhi), lwd = 0) +
+  geom_sf(data = country_sf, fill = NA, lwd = 0.1, color = "white") +
+  scale_fill_viridis_c(option = "magma") +
+  guides(fill = guide_legend(
+    title = "VHI (July-Aug)", 
+    label.theme = element_text(colour = "gray10", angle = 0, size = 8),
+    title.theme = element_text(colour = "gray10", angle = 0, size = 10))) +
+  theme_void()
 dev.off()
