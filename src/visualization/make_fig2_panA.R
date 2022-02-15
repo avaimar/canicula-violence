126a79fc7403ba666c5cd540fb4f16c99ff5c0b0
andreavallebueno <andreavalle.ai@gmail.com>
2022-02-14 18:29:57 -0800

    Modifications to Fig2 Pan A

--- src/visualization/make_fig2_panA.R
+++ src/visualization/make_fig2_panA.R
@@ -4,10 +4,11 @@ library(sf)
 library(tidyverse)
 library(fixest)
 library(dplyr)
+library(reshape2)
 
 gdrive_fpath <- "/Volumes/GoogleDrive/My Drive/Stanford/ESS 268/Project"
 model_data_fpath <- file.path(gdrive_fpath, "Models")
-figure_fpath <- file.path("results", "figures" )
+figure_fpath <- file.path(gdrive_fpath,  "Figures")
 
 load(file.path(model_data_fpath, "modelmatrix.RData"))
 
@@ -27,11 +28,11 @@ ggplot() +
 # Predict annual counterfactual for each municipality
 CF_YEAR <- 2017
 cf_df <- full_df
-cf_df <- cf_df %>% select(munic_dep, Year, Population, mean_vhi, hom_rate_100k)
+cf_df <- cf_df %>% dplyr::select(munic_dep, Year, Population, mean_vhi, hom_rate_100k)
 
 base_df <- cf_df %>% filter(Year == CF_YEAR) %>%
   mutate(base_vhi=mean_vhi) %>%
-  select(-c(mean_vhi, Population, hom_rate_100k, Year))
+  dplyr::select(-c(mean_vhi, Population, hom_rate_100k, Year))
 
 cf_df <- left_join(cf_df, base_df, by=c('munic_dep'))
 
@@ -63,17 +64,28 @@ annual_hom_cf <- cf_df %>%
 annual_hom_cf <- annual_hom_cf %>% 
   mutate(Scenario='Counterfactual') %>%
   mutate(homicides=homicides_cf) %>%
-  select(Year, homicides, Scenario)
+  dplyr::select(Year, homicides, Scenario)
 
 # Bind data for plotting
 final <- rbind(annual_hom, annual_hom_cf)
 
+finaldc <- reshape2::dcast(final, formula=Year ~ Scenario, value.var='homicides')
+
 vals <- c("Actual" = "grey47", "Counterfactual" = "lightcoral")
-ggplot(data=final, aes(x=Year, y=homicides, group=Scenario, colour=Scenario)) +
-  geom_line() +
+g1 <- ggplot() +
+  geom_line(data=final, aes(x=Year, y=homicides/1000, group=Scenario, colour=Scenario)) +
   theme_classic() + 
   theme(legend.position='bottom') + 
-  scale_color_manual(values=vals)
+  scale_color_manual(values=vals, name='') +
+  geom_ribbon(data=filter(finaldc, Year <=2017), 
+              aes(x=Year, ymin=Counterfactual/1000,ymax=Actual/1000), fill="indianred1", alpha=0.5)+
+  labs(y='')
+  #annotate("rect", xmin = 2013.9, xmax = 2014.1, 
+  #         ymin = min(final$homicides), 
+  #         ymax = max(final$homicides) * 1.05,
+  #         alpha = .1,fill = "blue")
+
+ggsave(file.path(figure_fpath, 'fig2_panA.png'), g1)
 
 ggplot(data=cf_df, aes(x=mean_vhi, group=Year))+ 
   geom_histogram(aes(group=Year)) + 
