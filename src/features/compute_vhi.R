b7dd17de56c9c94128a1d837015ef54949e318f1
Miki Verma <meverma@stanford.edu>
2022-02-28 20:56:20 -0800

    update vhi and chirps script

--- src/features/compute_vhi.R
+++ src/features/compute_vhi.R
@@ -4,15 +4,13 @@ library(exactextractr)
 library(tidyverse)
 
 #1. Set paths and parameters -----
-#gdrive data paths
-gdrive_fpath <- file.path( ##change this to point at google drive path
-  "/Volumes/GoogleDrive-112161833434429421879/My Drive/Project")
+load('CONFIG.Rspace') #load gdrive_fpath
 raw_data_fpath <- file.path(gdrive_fpath, "Data", "Raw")
 processed_data_fpath <- file.path(gdrive_fpath, "Data", "Processed")
 #repo data paths
 data_raw_VHI_fpath <- file.path("data_raw_local", "VHI" )
 figure_fpath <- file.path("results", "figures" )
-year_range <- c(2010:2020)
+year_range <- c(2011:2020)
 week_range <- c(27:35)
 
 
@@ -23,9 +21,10 @@ admin_bndry_sf <- st_read(
 #3.  Preprocess VHI Raster Data -----
 #read in vhi data during year/week_range and crop to 4 countries of interest
 vhi <- stack(
-  list.files(data_raw_VHI_fpath, pattern = ".tif$", full.names = T)) %>% 
+  list.files(data_raw_VHI_fpath, pattern = "*[^6].VH.VHI.tif$", full.names = T)) %>% 
   crop(extent(admin_bndry_sf))
 vhi  <- reclassify(vhi, cbind(-9999, NA)) #map -9999 to NULL
+
 #for each year, get mean value for each gridcell over week_range
 vhi_mean_by_year <- stackApply(
   vhi,
@@ -33,18 +32,20 @@ vhi_mean_by_year <- stackApply(
   fun = "mean",
   na.rm = T)
 names(vhi_mean_by_year) <-  paste("vhi", year_range, sep = ".")
-plot(vhi_mean_by_year$vhi.2011) #make sure this looks right
+plot(vhi_mean_by_year$vhi.2018) #make sure this looks right
 
 #4. Compute Average year VHI over spatial units----
 #get spatial average and convert VHI to between 0 and 1
 vhi_admin_bndry <- exact_extract(
   vhi_mean_by_year, admin_bndry_sf, fun = "mean") / 100
 admin_bndry_vhi_sf <- st_sf(cbind(admin_bndry_sf, vhi_admin_bndry))
-#reshape from wide to long format
+#reshape from wide to long format (vhi by year stored in last cols other than geom col)
 admin_bndry_vhi_sf <- gather(
-  admin_bndry_vhi_sf, Year, mean_vhi, (ncol(admin_bndry_sf) + 1):ncol(admin_bndry_vhi_sf)-1)
+  admin_bndry_vhi_sf, Year, mean_vhi, (ncol(admin_bndry_sf)+1):ncol(admin_bndry_vhi_sf) - 1)
 #extract portion of string containing the year
 admin_bndry_vhi_sf$Year <- as.numeric(substr(admin_bndry_vhi_sf$Year, 10, 14))
+admin_bndry_vhi_sf
+plot(filter(admin_bndry_vhi_sf, Year == 2018)['mean_vhi'])
 
 #5. Create Canicula Index Columns----
 #get canicula index by binning VHI by intensity according to FAO document
@@ -58,7 +59,7 @@ admin_bndry_vhi_sf$Year <- as.numeric(substr(admin_bndry_vhi_sf$Year, 10, 14))
 ## VHI_Bin: Interval of bin (factor)
 ## Canicula_Index: Canicula intensity index (1-5) smallest to largest (numerical)
 ## Canicula_Label: Canicula index label from "None" to "Extreme" (ordered factor)
-admin_bndry_vhi_sf <- admin_bndry_vhi_sf%>%
+admin_bndry_vhi_sf <- admin_bndry_vhi_sf %>%
   mutate(
     VHI_Bin = cut(mean_vhi, breaks = c(0,.25,.35, .38, .42, 1)),
     Canicula_Index = 5-as.numeric(cut(
@@ -73,5 +74,5 @@ admin_bndry_vhi_sf <- admin_bndry_vhi_sf%>%
 
 #6. Write out CSV ----
 write.csv(
-  st_drop_geometry(admin_bndry_vhi_sf), 
+  st_drop_geometry(select(admin_bndry_vhi_sf, -c(in_dry_corridor))), 
   file.path(processed_data_fpath, "Canicula_index_2010_2020.csv"))
--- src/visualization/chirps.r
+++ src/visualization/chirps.r
@@ -4,6 +4,7 @@ library(sp)
 library(exactextractr)
 library(tidyverse)
 library(RColorBrewer)
+library(zoo)
 
 # 1. Set paths and parameters -----
 # gdrive data paths
@@ -18,58 +19,45 @@ data_raw_CHIRPS_fpath <- file.path("data_raw_local", "CHIRPS")
 proj_crs <- 26716
 latlon_crs <- 4326
 
+year_range <- c(2010:2020)
+pentad_range <- c(1:72)
+
 # 2. Read in municipality/department shapefile  ----
 admin_bndry_sf <- st_read(
   file.path(processed_data_fpath, "cleaned_admin_boundaries.geojson")
 )
 
-#make country shapefile
-#@Ben: modify this if you want to plot time series at diff spatial agg
-country_sf <- admin_bndry_sf %>%
-  sf::st_transform(admin_bndry_sf, crs = proj_crs) %>%
-  group_by(shapeGroup) %>%
-  summarize(geometry = st_union(geometry)) %>%
-  sf::st_transform(admin_bndry_sf, crs = latlon_crs)
+
+filter(admin_bndry_sf, in_dry_corridor == T)
 
 # 3.  Read In  & Preprocess CHIRPS Raster Data -----
-# read in chirps data from .bil files (NOT THE .hdr file!!) 
-#a nd crop to our area of interest
 chirps <- stack(
   list.files(data_raw_CHIRPS_fpath, pattern = ".bil$", full.names = T, recursive = T)) %>%
-  crop(extent(country_sf))
+  crop(extent(filter(admin_bndry_sf, in_dry_corridor == T)))
 chirps <- reclassify(chirps, cbind(-9999, NA)) # map -9999 -> NULL
 
-nlayers(chirps) # number of 5 day intervals you have
-plot(chirps[[1:2]]) # visualize first two image to make sure it looks right
+prcp_pentad_year <- data.frame(rowMeans(t(getValues(chirps)), na.rm = TRUE))
+names(prcp_pentad_year) <- c('prcp')
+prcp_pentad_year$pentad <- rep(seq(length(pentad_range)), length(year_range))
+prcp_pentad_year$year <- rep(year_range, each = length(pentad_range))
+head(prcp_pentad_year)
 
-# get mean precip over country for each pentad-year
-chirps_country <- exact_extract(chirps, country_sf, fun = "mean")
-chirps_country_sf <- st_sf(cbind(country_sf, chirps_country))
-# reshape from wide to long format
-chirps_country_sf <- gather(
-  chirps_country_sf, layer_str, mean_precip, 
-  (ncol(country_sf) + 1):ncol(chirps_country_sf) - 1)
+prcp_pentad_year$week <- round(prcp_pentad_year$pentad*5/7)
 
-#Get year and pentad num
-# layer strings are coded like "mean.v2p0chirps201001" 
-n_chars <- nchar(chirps_country_sf$layer_str[1])
-# extract portion of string containing the year
-chirps_country_sf$year <- as.numeric(
-  substr(chirps_country_sf$layer_str, n_chars - 5, n_chars - 2))
-# extract portion of string containing the pentad
-chirps_country_sf$pentad <- as.numeric(
-  substr(chirps_country_sf$layer_str, n_chars - 1, n_chars))
-#drop layer string col
-chirps_country_sf <- chirps_country_sf%>% select(-layer_str)
+blue <- brewer.pal(9, name = "Blues")[9]
 
-head(chirps_country_sf)
-
-# 5.  Get yearly time series -----
-# plot time series for
-st_drop_geometry(chirps_country_sf) %>%
-  mutate(country.year = paste(shapeGroup, year, sep = ".")) %>%
+p <- prcp_pentad_year  %>%
+  filter(year > 2012) %>%
   ggplot(
-    aes(x = pentad, y = mean_precip,group = country.year,
-    colour = year, linetype = shapeGroup)) +
-  geom_line()
-
+  mapping = aes(x = week,y = prcp), color = blue, size = 1) +
+    stat_smooth(method="loess", span=0.1, se=TRUE, 
+    alpha=0.2, color =blue, fill =blue) +
+    annotate("rect", xmin = 26, xmax = 36, ymin = -Inf, ymax =Inf,
+            fill = "gray60", alpha = .3)
+p <- p + labs(x = "Week", y = "Precipitation (mm)")
+p + theme_bw() + 
+  theme(panel.border = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(), 
+  axis.line = element_line(colour = "black"))
+ggsave(file.path(figure_fpath, "prcp_canicula_2012_2020.pdf"), height = 3, width = 5)
